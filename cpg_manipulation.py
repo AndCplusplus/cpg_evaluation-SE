9e65d7b2a56555d8d2f96b0d82c104c65b82e8f7
gabreuhl <spacegabriel111@gmail.com>
2025-10-23 15:11:18 -0500

    added augmentator

--- cpg_manipulation.py
+++ cpg_manipulation.py
@@ -68,3 +68,21 @@ def visualize_graph(graph, feature):
     pos = nx.spring_layout(graph, seed=42)
     nx.draw(graph, pos, labels=labels, with_labels=True, ax=ax, node_color='orange', arrows=True)
     plt.show()
+
+def visualize_aug_graph(graph, feature, node_colors=None, default_color='cyan'):
+    labels = {node: data.get(feature, node) for node, data in graph.nodes(data=True)}
+    if node_colors is None:
+      node_colors = {}
+
+    color_map = [node_colors.get(node, default_color) for node in graph.nodes()]
+    fig, ax = plt.subplots(figsize=(10, 8))
+    pos = nx.spring_layout(graph, seed=42)
+    nx.draw(graph, pos, labels=labels, with_labels=True, ax=ax, node_color=color_map, arrows=True)
+    plt.show()
+
+def color_nodes(graph, vuln_caller):
+    node_colors = {}
+    for node, data in graph.nodes(data=True):
+        if data.get('METHOD_FULL_NAME:string') == vuln_caller:
+            node_colors[node] = 'red' 
+    return node_colors
