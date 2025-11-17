848a4113031fd528b74b3f50f4a0febb950e200c
Luis <77978673+OrangeXR@users.noreply.github.com>
2025-11-17 15:49:14 -0600

    Remove file type restriction for file upload
    Commented out file type restriction to allow any file upload.

--- gui.py
+++ gui.py
@@ -42,7 +42,7 @@ class VulnerabilityScannerApp:
 #        if self.file_path:
 #            messagebox.showinfo("File Selected", f"Selected: {self.file_path}")
         original_path = filedialog.askopenfilename(
-            filetypes=[("C Source Files", "*.c")]
+            #filetypes=[("C Source Files", "*.c")] # commented out to allow any file type to be uploaded
         )
         if original_path:
             ext = os.path.splitext(original_path)[1].lower()
